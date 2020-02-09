dc7b98824538670d2bee61f3813a0ce60ad16f90
moorcan <moorcan85@gmail.com>
2020-02-08 21:02:19 -0800

    added Deck.cs Scripts/Deck.cs

---
+++ BlackJack_game/Assets/Scripts/Deck.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    List<int> cards;
+
+    public void Shuffle()
+    {
+        if(cards == null)
+        {
+            cards = new List<int>();
+
+        }
+        else
+        {
+            cards.Clear();
+        }
+
+        //initializing the cards
+
+        for(int i = 0; i < 52; i++)
+        {
+            cards.Add(i);
+        }
+
+        //randon number chance value
+
+        int n = cards.Count;
+        while(n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            int temp = cards[k];
+            cards[k] = cards[n];
+            cards[n] = temp;
+        }
+    }
+    void Start()
+    {
+        Shuffle();
+    }
+
+    
+}
