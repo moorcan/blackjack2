7d8c787d1767314bea56161ccfb7af42ec2578c9
moorcan <moorcan85@gmail.com>
2020-02-08 18:21:37 -0800

    2020-02-08created card templates, and card flip scripts. I can't get the card to show on the screen when I press play.

---
+++ BlackJack_game/Assets/Scripts/DebugChangeCard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugChangeCard : MonoBehaviour
+{
+    CardModel cardModel;
+    int cardIndex = 0;
+    public GameObject card;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        cardModel = card.GetComponent<CardModel>();
+    }
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(10, 10, 100, 28), "Hit me!"))
+        {
+            cardModel.cardIndex = cardIndex;
+            cardModel.ToggleFace(true);
+
+            cardIndex++;
+
+            if(cardIndex == 52)
+            {
+                cardIndex = 0;
+
+                cardModel.ToggleFace(false);
+            }
+        }
+    }
+
+}
